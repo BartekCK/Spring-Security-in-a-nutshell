71770df30f9038040f410fb8f1e56ac574b03422
Bartek <bkotarski1@gmail.com>
2019-11-01 17:37:38 +0100

    Spring with JPA

---
+++ src/main/java/com/example/demo/DataWithSecurityApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import com.example.demo.repository.UserRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class DataWithSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataWithSecurityApplication.class, args);
+    }
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO USER(active, password, roles, username) VALUES (true, 'user', 'ROLE_ADMIN', 'user');
